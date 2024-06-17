c4159045593480ad02b101321cdbacd78f79f8d2
Italiano <mirandaquezadafabriciojoel@gmail.com>
2024-06-17 00:13:17 -0500

    Eventos & Binding

--- src/components/Eventos.tsx
+++ src/components/Eventos.tsx
@@ -6,18 +6,34 @@ export default class Eventos extends Component {
     this.state = {
       contador: 0,
     };
+
+    this.sumar = this.sumar.bind(this);
+    this.restar = this.restar.bind(this);
   }
-  sumar() {
-    
+  sumar(e) {
+    console.log("Sumando");
+    console.log(this);
+    this.setState({
+      contador: this.state.contador + 1,
+    });
   }
+
+  restar(e) {
+    console.log("Restando");
+    console.log(this);
+    this.setState({
+      contador: this.state.contador - 1,
+    });
+  }
+
   render(): ReactNode {
     return (
       <>
         <div>
           <h2>Eventos de Componentes de Clases</h2>
           <nav>
-            <button>+</button>
-            <button>-</button>
+            <button onClick={this.sumar}>+</button>
+            <button onClick={this.restar}>-</button>
           </nav>
           <h3>{this.state.contador}</h3>
         </div>
